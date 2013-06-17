bc2ccdfde9975e2a5a0c301c79ac72d1c2d911c5
BigBrother1984 <stevewatersy@gmail.com>
2013-06-17 03:15:54 +0200

    Halo: Add master switch to disable Halo (2/2) Also did some cleanup.
    Conflicts:
    	res/xml/interface_settings.xml

--- src/com/android/settings/carbon/InterfaceSettings.java
+++ src/com/android/settings/carbon/InterfaceSettings.java
@@ -95,6 +95,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment
     //private static final String PREF_SHOW_OVERFLOW = "show_overflow";
     private static final String KEY_BACKGROUND_PREF = "lockscreen_background";
     private static final String KEY_HARDWARE_KEYS = "hardware_keys";
+    private static final String KEY_HALO_ENABLED = "halo_enabled"
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
@@ -117,6 +118,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment
     //CheckBoxPreference mShowActionOverflow;
     ListPreference mCustomBackground;
     private ListPreference mHaloState;
+    private CheckBoxPreference mHaloEnabled;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
     private ListPreference mListViewAnimation;
@@ -183,6 +185,10 @@ public class InterfaceSettings extends SettingsPreferenceFragment
 
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+
+        mHaloEnabled = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_ENABLED);
+        mHaloEnabled.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_ENABLED, 0) == 1);
 
         mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
         mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
@@ -394,6 +400,10 @@ public class InterfaceSettings extends SettingsPreferenceFragment
                     Settings.System.NOTIFICATION_SHOW_WIFI_SSID,
                     mShowWifiName.isChecked() ? 1 : 0);
             return true;
+        } else if  (preference == mHaloEnabled) {
+             Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_ENABLED, mHaloEnabled.isChecked()
+                    ? 1 : 0);
         } else if (preference == mHaloHide) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_HIDE,
@@ -402,10 +412,6 @@ public class InterfaceSettings extends SettingsPreferenceFragment
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_REVERSED,
                     mHaloReversed.isChecked() ? 1 : 0);
-        /*} else if (preference == mUseAltResolver) {
-            Settings.System.putBoolean(getActivity().getContentResolver(),
-                    Settings.System.ACTIVITY_RESOLVER_USE_ALT,
-                    ((CheckBoxPreference) preference).isChecked());*/
             return true;
         }
 
