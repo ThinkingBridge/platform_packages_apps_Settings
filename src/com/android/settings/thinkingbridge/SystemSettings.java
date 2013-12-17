190467ce9a91d38becddc8d1481ca33125afc9f0
htchoi1995 <htchoi1995@gmail.com>
2013-12-18 00:27:27 +0900

    Settings: Bring back basic system settings
    and more translate for korean

---
+++ src/com/android/settings/thinkingbridge/SystemSettings.java
@@ -0,0 +1,18 @@
+package com.android.settings.thinkingbridge;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class SystemSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "SystemSettings";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.system_settings);
+    }
+}
