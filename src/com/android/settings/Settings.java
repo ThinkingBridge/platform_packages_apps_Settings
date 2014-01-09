44e00682866d21d74863f17a3b38ec23f9e62a01
htchoi1995 <htchoi1995@gmail.com>
2014-01-09 18:44:53 +0900

    Settings: Port APN Settings fix from cm11.0 credit @CyanogenMod

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -352,7 +352,8 @@ public class Settings extends PreferenceActivity
         PrintJobSettingsFragment.class.getName(),
         TrustedCredentialsSettings.class.getName(),
         PaymentSettings.class.getName(),
-        KeyboardLayoutPickerFragment.class.getName()
+        KeyboardLayoutPickerFragment.class.getName(),
+        ApnSettings.class.getName()
     };
 
     @Override
@@ -1135,4 +1136,6 @@ public class Settings extends PreferenceActivity
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
     public static class SystemSettingsActivity extends Settings { /* empty */ }
+    public static class ApnSettingsActivity extends Settings { /* empty */ }
+    public static class ApnEditorActivity extends Settings { /* empty */ }
 }
