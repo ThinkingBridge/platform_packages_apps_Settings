terciocarvalho/android_packages_apps_Settings

Settings: Favorite Contact Tile (2/2)
PSX: Create Dependant

Change-Id: I938490f3cfd40d1abff193315f5d885010386107

Patchset: 3
https://gerrit.slimroms.net/#/c/1728/

src/com/android/settings/temasek/quicksettings/QuickSettingsUtil.java
@@ -26,6 +26,7 @@ import static com.android.internal.util.temasek.QSConstants.TILE_BATTERY;
 import static com.android.internal.util.temasek.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.temasek.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.temasek.QSConstants.TILE_BUGREPORT;
+import static com.android.internal.util.temasek.QSConstants.TILE_CONTACT;
 import static com.android.internal.util.temasek.QSConstants.TILE_CUSTOM;
 import static com.android.internal.util.temasek.QSConstants.TILE_CUSTOM_KEY;
 import static com.android.internal.util.temasek.QSConstants.TILE_CUSTOM_DELIMITER;
@@ -162,6 +163,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CUSTOM, R.string.title_tile_custom,
                 "com.android.systemui:drawable/ic_qs_settings"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_CONTACT, R.string.title_tile_contact,
+                "com.android.systemui:drawable/ic_qs_default_user"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -309,6 +313,8 @@ Log.e("qsUtil", "torch removed");
                 Settings.System.CUSTOM_TOGGLE_EXTRAS, null);
         Settings.System.putString(context.getContentResolver(),
                 Settings.System.CUSTOM_TOGGLE_ACTIONS, null);
+        Settings.System.putString(context.getContentResolver(),
+                Settings.System.TILE_CONTACT_ACTIONS, null);
     }
 
     public static void deleteCustomTile(Context context, String tileKey) {
@@ -432,8 +438,7 @@ Log.e("qsUtil", "torch removed");
     }
 
     public static void saveCustomExtras(
-            Context context, String action, String tilekey) {
-        String setting = Settings.System.CUSTOM_TOGGLE_EXTRAS;
+            Context context, String action, String tilekey, String setting) {
         String oldSetting = Settings.System.getString(
                 context.getContentResolver(),
                 setting);

ThinkingBridge/platform_packages_apps_Settings

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -26,6 +26,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_BATTERY;
 import static com.android.internal.util.slim.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.slim.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.slim.QSConstants.TILE_BUGREPORT;
+import static com.android.internal.util.slim.QSConstants.TILE_CONTACT;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM_KEY;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM_DELIMITER;
@@ -162,6 +163,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CUSTOM, R.string.title_tile_custom,
                 "com.android.systemui:drawable/ic_qs_settings"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_CONTACT, R.string.title_tile_contact,
+                "com.android.systemui:drawable/ic_qs_default_user"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -309,6 +313,8 @@ Log.e("qsUtil", "torch removed");
                 Settings.System.CUSTOM_TOGGLE_EXTRAS, null);
         Settings.System.putString(context.getContentResolver(),
                 Settings.System.CUSTOM_TOGGLE_ACTIONS, null);
+        Settings.System.putString(context.getContentResolver(),
+                Settings.System.TILE_CONTACT_ACTIONS, null);
     }
 
     public static void deleteCustomTile(Context context, String tileKey) {
@@ -432,8 +438,7 @@ Log.e("qsUtil", "torch removed");
     }
 
     public static void saveCustomExtras(
-            Context context, String action, String tilekey) {
-        String setting = Settings.System.CUSTOM_TOGGLE_EXTRAS;
+            Context context, String action, String tilekey, String setting) {
         String oldSetting = Settings.System.getString(
                 context.getContentResolver(),
                 setting);